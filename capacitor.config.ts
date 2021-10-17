0388a9131b832beaf71ead94cad06478bbae61fe
Guadalupe <guadalupe.ines.alavila@everis.com>
2021-10-17 13:41:53 -0300

    update package name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.guadalavila.dev.movie',
   appName: 'movie-app',
   webDir: 'www',
   bundledWebRuntime: false
